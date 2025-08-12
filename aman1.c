23fa63ca297e26b207747e2b03a166d4030edf00
Aman-Raj-10 <amanrajx10@gmail.com>
2025-08-12 17:13:20 +0530

    Add files via upload

---
+++ aman1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+printf("enter number\n");
+int number;
+scanf("%d",&number);
+if(number%2==0)
+{
+printf("Number is Even");
+}
+else
+{
+printf("Number is Odd");
+}
+return 0;
+}
+
